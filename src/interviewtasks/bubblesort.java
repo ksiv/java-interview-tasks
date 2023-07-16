2e9d97142dee6c54ec8c4700a4614127350cb57e
ksiv <hellokonstantin@gmail.com>
2023-07-16 23:19:57 +0300

    added bubblesort.java

---
+++ src/interviewtasks/bubblesort.java
@@ -0,0 +1,54 @@
+package interviewtasks;
+
+import java.util.Arrays;
+
+
+/**
+ *
+ * Implementation of "bubble sort" algorithm
+ * TAGS: #sort
+ *
+ */
+public class bubblesort {
+
+
+    public static void main(String[] args) {
+        String helpMessage = "string like \"7,2,3,4,1,1\" expected as input.";
+        if (args.length != 1) {
+            System.out.println("wrong argument count");
+            System.out.println(helpMessage);
+            return;
+        } else {
+            // arg handling
+            String[] stringArray = args[0].split(",");
+            int [] arr = new int [stringArray.length];
+            for (int i=0;i<stringArray.length-1;i++) {
+
+                arr[i] = Integer.parseInt(stringArray[i]);
+
+            }
+
+            System.out.println("before: "+Arrays.toString(arr));
+            bubbleSort(arr);
+            System.out.println("after : "+Arrays.toString(arr));
+
+        }
+    }
+    public static void bubbleSort(int arr[])
+    {
+        Boolean swapDone=true;
+        int k;
+        while (swapDone){
+            swapDone=false;
+            for(int i=0;i<arr.length-1;i++){
+                k=i+1;
+                if (arr[i]>arr[k]){
+                    int tmpVar = arr[i];
+                    arr[i]=arr[k];
+                    arr[k]=tmpVar;
+                    swapDone=true;
+                }
+            }
+        }
+    }
+}
