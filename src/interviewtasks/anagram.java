b290ea2034d2ff91544c5122524b183318a77b32
ksiv <hellokonstantin@gmail.com>
2023-08-24 11:09:14 +0300

    added solution to anagram

--- src/interviewtasks/anagram.java
+++ src/interviewtasks/anagram.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 /**
  * \interviewtasks> java -classpath ".."  interviewtasks.anagram "asds", "ssda"
-
+ * <p>
  * According to wikipedia "An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once"
  * And "A word is a basic element of language that carries an objective or practical meaning"
  * Test tasks on interview in most cases exclude "carries an objective or practical meaning" part.
@@ -17,8 +17,7 @@ import java.util.Map;
  */
 
 public class anagram {
-    static String firstString;
-    static String secondString;
+
 
     public static void main(String[] args) {
         String helpMessage = "two strings is expected as an input";
@@ -27,11 +26,14 @@ public class anagram {
             System.out.println(helpMessage);
             return;
         } else {
+            String firstString;
+            String secondString;
             firstString = args[0];
             secondString = args[1];
             System.out.println(firstString + "_AND_" + secondString);
-            System.out.println(isAnagramViaArraySorting());
-            System.out.println(isAnagramViaSymbolCounters());
+            System.out.println(isAnagramViaArraySorting(firstString, secondString));
+            System.out.println(isAnagramViaSymbolCounters(firstString, secondString));
+            System.out.println(isAnagramViaIntArraySymbolCounters(firstString, secondString));
 
         }
     }
@@ -39,7 +41,7 @@ public class anagram {
     // Compare sorted arrays
     // Simple but could demand much RAM for big strings
     // Sorting also CPU intensive task
-    private static boolean isAnagramViaArraySorting() {
+    private static boolean isAnagramViaArraySorting(String firstString, String secondString) {
 
         // Diff length strings can not be anagram
         if (firstString.length() != secondString.length()) {
@@ -64,7 +66,7 @@ public class anagram {
     // Char frequency counters
     // encounters in the first string and Lower for second one
     // For an anagram the index must be empty by the end
-    private static boolean isAnagramViaSymbolCounters() {
+    private static boolean isAnagramViaSymbolCounters(String firstString, String secondString) {
 
         // Diff length strings can not be anagram
         if (firstString.length() != secondString.length()) {
@@ -85,7 +87,7 @@ public class anagram {
             // If the symbol in the map reducing counter otherwise exit(false)
             if (!symbolFrequency.containsKey(tmpChar)) {
                 return false;
-            }else{
+            } else {
                 symbolFrequency.put(tmpChar, symbolFrequency.get(tmpChar) - 1);
             }
             // Removing element if same amount of this symbol has been found in both string
@@ -99,4 +101,33 @@ public class anagram {
 
     }
 
+    public static boolean isAnagramViaIntArraySymbolCounters(String s, String t) {
+        // Diff length strings can not be anagram
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] map = new int[128];
+        char tChar, sChar;
+        // Traverse the first string and rise counters for each symbol
+        // Traverse the second string decreasing counters
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sChar = s.charAt(i);
+            tChar = t.charAt(i);
+            map[sChar]++;
+            map[tChar]--;
+
+        }
+
+        for (int val : map) {
+            if (val != 0) return false;
+        }
+        // check if Map is empty
+
+        return true;
+
+
+    }
+
 }
+
+
