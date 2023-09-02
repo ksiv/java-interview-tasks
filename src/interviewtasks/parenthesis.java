584cf7be50c427303dfae0147d669c85304fa948
ksiv <hellokonstantin@gmail.com>
2023-09-02 22:40:18 +0300

    added info

--- src/interviewtasks/parenthesis.java
+++ src/interviewtasks/parenthesis.java
@@ -2,6 +2,17 @@ package interviewtasks;
 
 import java.util.*;
 
+/**
+ * leetcode 20. valid parenthesis
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ *
+ */
 public class parenthesis {
     static class Solution {
         public boolean isValid(String s) {
