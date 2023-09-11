b2cd4ac9f1ad44e2635e72b2cde072115a6d60e3
ksiv <hellokonstantin@gmail.com>
2023-09-11 11:03:34 +0300

    minor info

--- src/interviewtasks/kadanemaxsubbaray.java
+++ src/interviewtasks/kadanemaxsubbaray.java
@@ -9,6 +9,14 @@ public class kadanemaxsubbaray {
     static class Solution {
         public static int maxSubArray(int[] nums) {
             // Kadane's implementation
+            /**
+             * Say array is: 1,2,3,-7,1,2,4
+             *               ↓ ↓ ↓  ↓ ↓ ↓ ↓
+             * maxAtIndex    1 3 6 -1 1 3 7
+             *               ↓ ↓ ↓  ↓ ↓ ↓ ↓
+             * max           1 3 6  6 6 6 7
+             */
+
             int maxSum = nums[0];
             int maxAtIndex = nums[0];
             for (int i = 1; i < nums.length; i++) {
